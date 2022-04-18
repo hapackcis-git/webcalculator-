10fac2815e64eb96ff73ca58cb5b2783ec61deb0
Hapack <hapackcis@gmail.com>
2022-04-19 00:24:08 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,30 @@
+let OutputScreen=document.getElementById("output-screen");
+
+function display(num)
+{
+    OutputScreen.value +=num;
+}
+
+function Calculate()
+{
+   try{
+               OutputScreen.value=eval(OutputScreen.value);
+    
+   }
+   catch(err)
+   {
+       alert("Invalid")
+
+   }
+   
+}
+
+
+function Clear()
+{
+OutputScreen.value="";
+}
+function backspace()
+{
+    OutputScreen.value=OutputScreen.value.slice(0,-1);
+}
